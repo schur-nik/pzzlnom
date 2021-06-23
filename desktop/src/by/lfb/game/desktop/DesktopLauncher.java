915c485209f05cd0f1c512d27e8ddc5081afcd7d
Shyr_NS <schur.nik@mail.ru>
2021-06-23 18:21:44 +0300

    Initial commit

---
+++ desktop/src/by/lfb/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package by.lfb.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import by.lfb.game.GameScreen;
+import by.lfb.game.Pzzlnom;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Pzzlnom";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Pzzlnom(), config);
+	}
+}
